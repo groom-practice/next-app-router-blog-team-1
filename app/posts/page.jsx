e895b66a36b1f9dc76029f2102ef5fff60bd60af
SNXWXH <snxwxh@gmail.com>
2025-05-20 11:23:26 +0900

    feat: 검색창 컴포넌트 생성 및 구현

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import SearchBar from '@/components/SearchBar';
 
 async function getPosts() {
   const res = await fetch('http://localhost:3000/api/posts', {
@@ -10,40 +11,49 @@ async function getPosts() {
 export default async function PostLists({ searchParams }) {
   const postList = await getPosts();
   const categories = ['All', 'React', 'Next.js', 'JavaScript'];
-  const category = searchParams?.category || '';
+  const category = searchParams?.category || 'All';
+  const keyword = searchParams?.query || '';
 
   const filterPosts = postList.filter((post) => {
-    if (!category || category === 'All') return true;
-    return post.category === category;
+    const title = post.title.toLowerCase();
+    const search = keyword.toLowerCase();
+
+    if (!title.includes(search)) return false;
+    if (category && category !== 'All' && post.category !== category)
+      return false;
+
+    return true;
   });
 
   return (
     <div className='m-4'>
       <p className='text-xl font-bold mb-2'>글 목록</p>
+      <SearchBar />
 
       <div className='flex gap-2 my-2'>
-        {categories.map((category) => (
+        {categories.map((cat) => (
           <Link
-            key={category}
-            href={category === 'All' ? '/posts' : `posts/?category=${category}`}
+            key={cat}
+            href={cat === 'All' ? '/posts' : `/posts/?category=${cat}`}
             className='text-blue-500 underline'
           >
-            {category}
+            {cat}
           </Link>
         ))}
       </div>
-      {filterPosts.map((post) => {
-        return (
-          <li key={post.id} className='text-lg list-none py-1  border-b'>
-            <Link href={`posts/${post.id}`} className=' text-blue-500'>
+
+      <ul>
+        {filterPosts.map((post) => (
+          <li key={post.id} className='text-lg list-none py-1 border-b'>
+            <Link href={`posts/${post.id}`} className='text-blue-500'>
               {post.title}
             </Link>
             <span className='text-sm text-gray-500 ml-1'>
               [{post.category}]
             </span>
           </li>
-        );
-      })}
+        ))}
+      </ul>
     </div>
   );
 }
