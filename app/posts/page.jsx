a0d6e6594d1069a092afcf74a890fbfcf8bee2b3
SNXWXH <snxwxh@gmail.com>
2025-05-20 11:00:30 +0900

    feat: 카테고리 필터 기능 구현

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -7,19 +7,40 @@ async function getPosts() {
   return res.json();
 }
 
-export default async function PostLists() {
+export default async function PostLists({ searchParams }) {
   const postList = await getPosts();
+  const categories = ['All', 'React', 'Next.js', 'JavaScript'];
+  const category = searchParams?.category || '';
+
+  const filterPosts = postList.filter((post) => {
+    if (!category || category === 'All') return true;
+    return post.category === category;
+  });
 
   return (
     <div className='m-4'>
       <p className='text-xl font-bold mb-2'>글 목록</p>
 
-      {postList.map((post) => {
+      <div className='flex gap-2 my-2'>
+        {categories.map((category) => (
+          <Link
+            key={category}
+            href={category === 'All' ? '/posts' : `posts/?category=${category}`}
+            className='text-blue-500 underline'
+          >
+            {category}
+          </Link>
+        ))}
+      </div>
+      {filterPosts.map((post) => {
         return (
-          <li key={post.id} className='text-lg list-none text-blue-500 py-1'>
-            <Link href={`posts/${post.id}`} className=' border-b'>
+          <li key={post.id} className='text-lg list-none py-1  border-b'>
+            <Link href={`posts/${post.id}`} className=' text-blue-500'>
               {post.title}
             </Link>
+            <span className='text-sm text-gray-500 ml-1'>
+              [{post.category}]
+            </span>
           </li>
         );
       })}
