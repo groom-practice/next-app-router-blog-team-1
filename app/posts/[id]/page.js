55b27faf38f3bd149e26063d6cc12527cd7f6b8e
“hayuhee” <weeeerfriend@gmail.com>
2025-05-20 11:37:17 +0900

    feat/글 상세페이지 수정

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,8 +1,8 @@
 "use client"
 
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { use, useEffect, useState } from "react";
 
 export default function PostDetailPage({ params }) {
   const { id } = use(params);
