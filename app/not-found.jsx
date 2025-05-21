a010ae3167b5b490d37daea70995a606f51c0497
seung <monitz@naver.com>
2025-05-21 10:30:04 +0900

    feat:글 작성 페이지 경로 설정

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,9 +1,20 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 export default function NotFoundPage() {
+  const router = useRouter();
+
   return (
-    <div>
-      <h1 className="text-2x1 font-bold text-red-600">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white text-center">
+      <h1 className="text-3xl font-bold text-red-600 mb-2">
         404 - 페이지를 찾을 수 없습니다
       </h1>
+      <button
+        onClick={() => router.push("/")}
+        className="mt-6 px-4 py-2 bg-blue-500 text-white rounded cursor-pointer hover:bg-blue-600"
+      >
+        홈으로 돌아가기
+      </button>
     </div>
   );
 }
